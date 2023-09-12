382d75e90259cdb2f93b20f0bde7e7661e42fcf8
Paul Cleary <pcleary@tunein.com>
2023-09-12 09:09:41 -0700

    add dockerfile and fix gha

--- apps/discord-bot/main.py
+++ apps/discord-bot/main.py
@@ -10,25 +10,6 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-# async def get_animal_types(ctx: discord.AutocompleteContext):
-#   """
-#   Here we will check if 'ctx.options['animal_type']' is a marine or land animal and return respective option choices
-#   """
-#   animal_type = ctx.options['animal_type']
-#   if animal_type == 'Marine':
-#     return ['Whale', 'Shark', 'Fish', 'Octopus', 'Turtle']
-#   else: # is land animal
-#     return ['Snake', 'Wolf', 'Lizard', 'Lion', 'Bird']
-
-# @bot.slash_command(name="animal")
-# async def animal_command(
-#   ctx: discord.ApplicationContext,
-#   animal_type: discord.Option(str, choices=['Marine', 'Land']),
-#   animal: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_animal_types))
-# ):
-#   await ctx.respond(f'You picked an animal type of `{animal_type}` that led you to pick `{animal}`!')
-
-
 @bot.slash_command(name="nasa")
 async def nasa_command(
   ctx: discord.ApplicationContext,
@@ -43,7 +24,6 @@ async def nasa_command(
     await ctx.respond(f"Astronomy Picture of the Day: {apod_data['title']} - {apod_data['date']}\n{apod_data['explanation']}\n{apod_data['hdurl']}")
   else: 
     pass
-
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
